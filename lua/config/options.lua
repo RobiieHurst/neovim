e0271f2b4c3fe0c7f3707597acd3a7c0969f4e99
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-02 20:23:41 +0200

    Update Snacks dashboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,1 @@
-vim.g.snacks_animate = false
+vim.g.lazyvim_prettier_needs_config = true
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    opts = {
+      modes = {
+        symbols = {
+          win = { position = "bottom" },
+        },
+      },
+    },
+  },
+}
