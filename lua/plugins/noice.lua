ce47fbf686066eda67f023884128d1255648f6eb
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-01-07 11:09:35 +0200

    Update obsidian/noice cmdline

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline",
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+    },
+  },
+}
