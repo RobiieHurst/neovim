9a9ba0e8b66a69b0b27f2dc990b079253bb1b2c6
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-02-27 13:39:55 +0200

    Update set

--- lua/rob/set.lua
+++ lua/rob/set.lua
@@ -20,11 +20,11 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
--- vim.opt.termguicolors = true
+vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
--- vim.opt.isfname:append("@-@")
+vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
